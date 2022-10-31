5837cb416a78e30cacfcd1e907784cff1b2fd42e
SurajGayak <nepsuraj12@gmail.com>
2022-10-31 15:44:20 +0545

    quick fix

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-    <x-header activeLink='about'/>
+    <x-header activeLink='contact'/>
     <div class="min-h-screen bg-blue-100">
         <p class="text-center">This is the contact page</p>
     </div>
