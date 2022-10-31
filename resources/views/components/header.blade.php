cc8e3df3a0b833380c368914823a4484eabad721
Ranjan <khanalranjan43@gmail.com>
2022-10-31 15:27:06 +0545

    Added route for contact

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,8 +3,8 @@
         <a href="/home" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark">Home</a>
     </div>
     <div>
-        <a href="/one" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">One</a>
-        <a href="/two" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Two</a>
+        <a href="/one" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">About</a>
+        <a href="/contact" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Contact</a>
         <a href="/three" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Three</a>
     </div>
 </nav>
